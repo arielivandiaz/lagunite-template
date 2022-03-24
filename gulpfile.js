6aae9ef4b0cd69025732257e638eac9b88da42c1
Ariel I. Diaz <arielivandiaz@gmail.com>
2022-03-24 11:08:02 -0300

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var autoprefixer = require('gulp-autoprefixer');
 const minify = require('gulp-minify');
 const cleanCSS = require("gulp-clean-css");
 var uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 var concatJS = require('gulp-concat');
 var concatCss = require('gulp-concat-css');
 const purgecss = require('gulp-purgecss');
@@ -10,9 +11,11 @@ var htmlmin = require('gulp-htmlmin');
 var htmlreplace = require('gulp-html-replace');
 const fileinclude = require('gulp-file-include');
 const imagemin = require('gulp-imagemin');
+const headerComment = require('gulp-header-comment');
 
 var fs = require('fs');
-var file_path = 'version.json';
+var file_path = 'package.json';
+
 
 
 
@@ -38,43 +41,55 @@ var pathsImgs = {
 
 
 
-let getVersion = async () => {
+let getVersion = () => {
     return new Promise((resolve, reject) => {
         fs.readFile(file_path, 'utf8', function readFileCallback(err, data) {
             if (err) {
                 console.log(err);
                 reject(err);
             } else {
-                obj = JSON.parse(data);
-                obj.rev += 1;
-                json = JSON.stringify(obj);
-                fs.writeFile(file_path, json, 'utf8', () => {
-                    console.log("Updated version: " + obj.version + "." + obj.rev);
-                    resolve(obj.version + "." + obj.rev);
-                });
+                var obj = JSON.parse(data);
+                resolve(obj.version);
             }
         });
     });
 }
 
-gulp.task('css', () => {
-    return gulp.src(pathsCSS.source)
-        .pipe(concatCss("bundle.css"))
-        .pipe(purgecss({
-            content: ['*.html']
-        }))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())
-        .pipe(minify())
-        .pipe(gulp.dest(pathsCSS.destination));
+
+
+gulp.task('css', async () => {
+    getVersion().then((v) => {
+        return gulp.src(pathsCSS.source)
+            .pipe(concatCss("bundle.css"))
+            .pipe(purgecss({
+                content: ['*.html']
+            }))
+            .pipe(autoprefixer())
+            .pipe(cleanCSS())
+            .pipe(minify())
+            .pipe(headerComment('Lagunite v' + v + '\n\r Visit lagunite.com'))
+            .pipe(gulp.dest(pathsCSS.destination));
+    }).catch((reject) => {
+        console.log("File Read Failed");
+        console.log(reject);
+    });
 });
 
 
-gulp.task('js', () => {
-    return gulp.src(pathsJS.source)
-        .pipe(concatJS('scripts.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(pathsJS.destination));
+gulp.task('js', async () => {
+    getVersion().then((v) => {
+        return gulp.src(pathsJS.source)
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(concatJS('scripts.js'))
+            .pipe(uglify())
+            .pipe(headerComment('Lagunite v' + v + '\n\r Visit lagunite.com'))
+            .pipe(gulp.dest(pathsJS.destination));
+    }).catch((reject) => {
+        console.log("File Read Failed");
+        console.log(reject);
+    });
 });
 
 
@@ -102,7 +117,7 @@ gulp.task('html', async () => {
 });
 
 
-gulp.task('img', () => {
+gulp.task('img', async () => {
     return gulp.src(pathsImgs.source)
         .pipe(imagemin())
         .pipe(gulp.dest(pathsImgs.destination));
